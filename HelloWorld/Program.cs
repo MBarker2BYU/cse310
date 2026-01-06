55f03c8f21e77c75b2c856ca923a326fe01939e0
MBarker2BYU <mbarker2@byupathway.edu>
2026-01-05 20:58:26 -0500

    Hello World Checkin

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello World!");
+Console.WriteLine();
+Console.WriteLine("Semper Fi from Matthew Barker");
+Console.WriteLine("Architect & Senior Software Engineer | 25+ years experience");
+Console.WriteLine("USMC Veteran | NRA Certified Firearms Instructor | Sig Sauer Armorer");
+Console.WriteLine("BASc Software Development – BYU-Pathway");
+Console.WriteLine();
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();

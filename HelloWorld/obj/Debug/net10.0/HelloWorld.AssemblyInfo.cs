54d2f02e3aedbde29461f0e36e2ae53c5ccfa78f
ShadowWorx <mdbarker813@att.net>
2026-01-08 07:21:43 -0500

    Added some spacing

--- HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50b1b655ddafd140ae60c7253e574053b7b6daef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c194f53a3eabd4b9382c27ec70bb89ec15208cde")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

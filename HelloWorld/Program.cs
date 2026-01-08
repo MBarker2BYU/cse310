4e2889c61551c1bf35203ffdf763c80baf53778d
ShadowWorx <mdbarker813@att.net>
2026-01-08 07:31:02 -0500

    Updated Info

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.WriteLine("Hello World!");
 Console.WriteLine();
-Console.WriteLine("Semper Fi from Matthew Barker");
-Console.WriteLine("Architect & Senior Software Engineer | 25+ years experience");
+Console.WriteLine("Semper Fi from Matthew Barker!");
+Console.WriteLine("CEO of ShadowWorx Systems | Architect & Senior Software Engineer | 30+ years experience");
 Console.WriteLine("USMC Veteran | NRA Certified Firearms Instructor | Sig Sauer Armorer");
-Console.WriteLine("BASc Software Development – BYU-Pathway");
+Console.WriteLine("BASc Software Development – BYU-Idaho");
 Console.WriteLine();
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
